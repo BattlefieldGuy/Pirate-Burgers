772a7294ee479bca70399bf1d91ad46b2207d468
BattlefieldGuy <matthewklijnstra@gmail.com>
2025-11-20 11:58:39 +0100

    Bonnetjes setup en Scene Setup

---
+++ Assets/_DEV/Matthew/Scripts/BonnetjesManager.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BonnetjesManager : MonoBehaviour
+{
+    // Variables
+    public List<string> Ingridients = new List<string>();
+
+
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
