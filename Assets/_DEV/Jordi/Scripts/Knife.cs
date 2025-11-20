80a77b8571014b4115cf79f997e2d80a991e0f00
Destroliedel22 <Jordiwalet@gmail.com>
2025-11-20 11:16:05 +0100

    xr project settings added

--- Assets/_DEV/Jordi/Scripts/Knife.cs
+++ Assets/_DEV/Jordi/Scripts/Knife.cs
@@ -1,21 +1,16 @@
+using System.Linq;
 using UnityEngine;
 
 public class Knife : MonoBehaviour
 {
-    [SerializeField] private GameObject saladPiecePrefab;
-
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.transform.CompareTag("Sliceable"))
         {
-            //slice in half
-            for(int i = 0; i < 2; i++)
-            {
-                GameObject clone = Instantiate(saladPiecePrefab, collision.transform.position, collision.transform.rotation);
-                collision.gameObject.SetActive(false);
-                clone.transform.parent = null;
-                clone.transform.localScale = new Vector3(.25f, .25f, .25f);
-            }
+            //get the script inside the sliceable object with the interface ISliceable on it
+            ISliceable _sliceable = collision.gameObject.GetComponents<MonoBehaviour>().OfType<ISliceable>().FirstOrDefault();
+            //if not null call OnSlice
+            _sliceable?.OnSlice();
         }
     }
 }
