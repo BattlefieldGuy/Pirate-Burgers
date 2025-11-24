2de057131138928c36e78a6882d4b95f0aed272a
BattlefieldGuy <matthewklijnstra@gmail.com>
2025-11-24 12:04:01 +0100

    Bonnetjes manager update
    added more variables

--- Assets/_DEV/Matthew/Scripts/BonnetjesManager.cs
+++ Assets/_DEV/Matthew/Scripts/BonnetjesManager.cs
@@ -3,8 +3,34 @@ using UnityEngine;
 
 public class BonnetjesManager : MonoBehaviour
 {
-    // Variables
-    public List<string> Ingridients = new List<string>();
+    // Ingridients
+    public List<string> MainIngridients = new List<string>();
+
+    public List<string> SecondaryIngridients = new List<string>();
+
+    // Customers Variables
+    public int CurrentCustomers = 0;
+
+    [SerializeField] private int maxCustomers = 5;
+
+    // Custom Variables
+    [SerializeField] private float spawnIntervalMin = 10f;
+    [SerializeField] private float spawnIntervalMax = 15f;
+
+    // Money
+    [SerializeField] private float customerMoneyValue;
+    [SerializeField] private float customerMoneyMin = 10f;
+    [SerializeField] private float customerMoneyMax = 10f;
+
+    // Patience
+    [SerializeField] private float customerPatienceValue;
+    [SerializeField] private float customerPatienceMin = 30f;
+    [SerializeField] private float customerPatienceMax = 60f;
+
+    // Pickiness
+    [SerializeField] private float customerPickinessValue;
+    [SerializeField] private float customerPickinessMin = 0.1f;
+    [SerializeField] private float customerPickinessMax = 0.5f;
 
 
 
@@ -16,5 +42,11 @@ public class BonnetjesManager : MonoBehaviour
     void Update()
     {
 
+    }
+
+
+    public void MakeItem()
+    {
+        // Create a new item with random ingridients
     }
 }
