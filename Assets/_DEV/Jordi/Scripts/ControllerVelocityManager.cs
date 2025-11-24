e71153a798f95681695e0a6c148d39c369895a32
Destroliedel22 <Jordiwalet@gmail.com>
2025-11-24 11:46:36 +0100

    knife can only cut if velocity is above threshold

---
+++ Assets/_DEV/Jordi/Scripts/ControllerVelocityManager.cs
@@ -0,0 +1,60 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class ControllerVelocityManager : MonoBehaviour
+{
+    public static ControllerVelocityManager Instance;
+
+    public Vector3 Velocity;
+    public bool LeftHanded;
+
+    private InputDevice leftDevice;
+    private InputDevice rightDevice;
+
+    private void Awake()
+    {
+        if(Instance == null)
+            Instance = this;
+        else
+            Destroy(this.gameObject);
+    }
+
+    void Start()
+    {
+        FindDevices();
+    }
+
+    private void Update()
+    {
+        if (!leftDevice.isValid || !rightDevice.isValid)
+        {
+            FindDevices();
+        }
+
+        if (!LeftHanded)
+            CheckVelocity(rightDevice);
+        else
+            CheckVelocity(leftDevice);
+    }
+
+    private void FindDevices()
+    {
+        // Find devices on LeftHand / RightHand
+        var leftHanded = new List<InputDevice>();
+        var rightHanded = new List<InputDevice>();
+        InputDevices.GetDevicesAtXRNode(XRNode.LeftHand, leftHanded);
+        InputDevices.GetDevicesAtXRNode(XRNode.RightHand, rightHanded);
+
+        if (leftHanded.Count > 0) leftDevice = leftHanded[0];
+        if (rightHanded.Count > 0) rightDevice = rightHanded[0];
+    }
+
+    private void CheckVelocity(InputDevice device)
+    {
+        if (device.TryGetFeatureValue(CommonUsages.deviceVelocity, out Velocity))
+        {
+
+        }
+    }
+}
