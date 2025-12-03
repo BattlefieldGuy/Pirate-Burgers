584818552d8e6a3f553a18422ac74550370b7d89
pollyamory <144798759+Polly224@users.noreply.github.com>
2025-12-03 13:13:16 +0100

    Updated CustomerPosManager.cs script

--- Assets/_DEV/Laura/Scripts/CustomerPosManager.cs
+++ Assets/_DEV/Laura/Scripts/CustomerPosManager.cs
@@ -9,13 +9,15 @@ using static UnityEngine.InputSystem.InputAction;
 
 public class CustomerPosManager : MonoBehaviour
 {
-    // Script that manages customer positions when they first come up to the window.
+    // Script that manages customer positions when they first come up to the window. Attach this to a GameObject in the scene,
+    // then call functions from said object whenever a customer is added/removed.
 
     // Reference position for where the frontmost leftmost customer will be.
     [SerializeField] private Vector3 basePosition = Vector3.zero;
 
     // The amount of customers per row.
     private static int ROWCUSTOMERLIMIT = 3;
+    private static int TOTALCUSTOMERLIMIT = 10;
 
     // Horizontal distance between each customer, and longitudal distance between each customer row.
     [SerializeField] [Range(0, 5)] private float xBaseDistance;
@@ -26,9 +28,9 @@ public class CustomerPosManager : MonoBehaviour
     [SerializeField] [Range(0, 5)] private float zMinRandomness;
     [SerializeField] [Range(0, 5)] private float zMaxRandomness;
 
-    // The list of where each customer should go.
-    private List<Vector3> customerPositions = new();
+    // List of customers, and a list of the positions made for spawned customers.
     private List<GameObject> spawnedCustomers = new();
+    private List<Vector3> customerPositions = new();
 
     // Testing variable and functions, should be managed by other customer scripts in the final game.
     [SerializeField] private GameObject customerPrefab;
@@ -49,6 +51,8 @@ public class CustomerPosManager : MonoBehaviour
         }
     }
 
+    // Actual scripts:
+
     private void FixedUpdate()
     {
         // Smoothly moves every customer to their respective waiting position.
@@ -59,32 +63,46 @@ public class CustomerPosManager : MonoBehaviour
         }
     }
 
-    public void AddNewCustomer(GameObject customerObject)
+    public bool AddNewCustomer(GameObject customerObject)
     {
         // Adds a created customer to the list of customers to manage. Make a customer object first, then assign it through this.
-        customerPositions.Add(GetNewCustomerPosition());
-        spawnedCustomers.Add(customerObject);
+        if(TOTALCUSTOMERLIMIT > spawnedCustomers.Count)
+        {
+            customerPositions.Add(GetNewCustomerPosition());
+            spawnedCustomers.Add(customerObject);
+
+            // If there's room to add a new customer, returns true. Else, returns false.
+            return true;
+        }
+        return false;
     }
 
-    public void RemoveCustomer(GameObject customerObject)
+    public bool RemoveCustomer(GameObject customerObject)
     {
         // Whenever a customer is done ordering and wants to leave, remove them with this script. Do this BEFORE you destroy
         // their GameObject, otherwise this function won't be able to properly remove them and move the line along.
-        int _totalCount = spawnedCustomers.Count;
-        int _counter = spawnedCustomers.IndexOf(customerObject);
-        while(_counter + ROWCUSTOMERLIMIT < _totalCount)
+        if (spawnedCustomers.Contains(customerObject))
         {
-            spawnedCustomers[_counter] = spawnedCustomers[_counter + ROWCUSTOMERLIMIT];
-            _counter += ROWCUSTOMERLIMIT;
+            int _totalCount = spawnedCustomers.Count;
+            int _counter = spawnedCustomers.IndexOf(customerObject);
+            while(_counter + ROWCUSTOMERLIMIT < _totalCount)
+            {
+                spawnedCustomers[_counter] = spawnedCustomers[_counter + ROWCUSTOMERLIMIT];
+                _counter += ROWCUSTOMERLIMIT;
+            }
+            if(_counter % ROWCUSTOMERLIMIT < (float)ROWCUSTOMERLIMIT / 2f)
+                spawnedCustomers.RemoveAt(_counter);
+            else
+            {
+                spawnedCustomers[_counter] = spawnedCustomers[^1];
+                spawnedCustomers.RemoveAt(spawnedCustomers.Count - 1);
+            }
+            customerPositions.RemoveAt(customerPositions.Count - 1);
+
+            // Returns whether the customer was removed successfully.
+            return true;
         }
-        if(_counter % ROWCUSTOMERLIMIT < (float)ROWCUSTOMERLIMIT / 2f)
-            spawnedCustomers.RemoveAt(_counter);
-        else
-        {
-            spawnedCustomers[_counter] = spawnedCustomers[^1];
-            spawnedCustomers.RemoveAt(spawnedCustomers.Count - 1);
-        }
-        customerPositions.RemoveAt(customerPositions.Count - 1);
+        return false;
     }
 
     private Vector3 GetNewCustomerPosition()
