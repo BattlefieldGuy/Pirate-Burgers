91fa1f7f02408b250933247d16c2b54f4bc4caf4
pollyamory <144798759+Polly224@users.noreply.github.com>
2026-01-06 16:25:34 +0100

    RunManager script
    Attach this to any object. When an order is finished, good or bad, call the RunManager.RecordOrder() function, which is static, so you don't need to get the actual script in your own script.

---
+++ Assets/_DEV/Laura/Scripts/RunManager.cs
@@ -0,0 +1,121 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RunManager : MonoBehaviour
+{
+    [Header("Timer variables"), SerializeField]
+    private float dayLengthInMinutes = 20f;
+
+    private float timeInSeconds;
+
+    public static bool dayStarted = false, dayEnded = false;
+
+    // Run variables
+    private static int totalOrders = 0, goodOrders = 0, badOrders = 0;
+    
+    private EndOfDayFeedback feedbackScript;
+
+    #region --- UNITY METHODS ---
+
+    void Start()
+    {
+        feedbackScript = FindFirstObjectByType<EndOfDayFeedback>();
+        StartDay();
+    }
+
+    void Update()
+    {
+        if(dayStarted && !dayEnded)
+        {
+            CountdownEvent();
+            CountdownChecks();
+        }
+    }
+
+    #endregion
+
+    #region --- TIMER FUNCTIONS ---
+
+    private void CalculateTimeInSeconds() => timeInSeconds = dayLengthInMinutes * 60;
+
+    public void StartDay()
+    {
+        CalculateTimeInSeconds();
+        dayStarted = true;
+    }
+
+    private void CountdownEvent()
+    {
+        if (timeInSeconds > 0)
+            timeInSeconds -= Time.deltaTime;
+    }
+
+    private void CountdownChecks()
+    {
+        if(timeInSeconds <= 0)
+        {
+            dayEnded = true;
+            OnDayEnd();
+        }
+        if(timeInSeconds <= dayLengthInMinutes * 30)
+        {
+            OnDayHalfway();
+        }
+    }
+
+    private void OnDayHalfway()
+    {
+        // Odds are this function isn't necessary, but if you want things to happen the moment the day is 50% done,
+        // put that code here. Like Parrot voicelines for example, if we somehow manage to cram those in, lol.
+    }
+
+    private void OnDayEnd()
+    {
+        if (feedbackScript != null)
+        {
+            feedbackScript.TotalOrders = totalOrders;
+            feedbackScript.GoodOrders = goodOrders;
+            feedbackScript.BadOrders = badOrders;
+            feedbackScript.enabled = true;
+        }
+        else Debug.LogError("There is no EndOfDayFeedback component in the scene to log results to!");
+    }
+
+    #endregion
+
+    #region --- DATA MANAGEMENT ---
+
+    // After an order is finished, call RunManager.RecordOrder() to save it.
+    public static void RecordOrder(bool goodOrder)
+    {
+        totalOrders++;
+        if (goodOrder)
+            goodOrders++;
+        else badOrders++;
+    }
+
+    // If you need to get the recorded orders, call RunManager.GetOrderRecords().
+    public static int[] GetOrderRecords()
+    {
+        return new int[3]
+        {
+            totalOrders,
+            goodOrders,
+            badOrders
+        };
+    }
+
+    #endregion
+
+
+    #region - DEBUGGING -
+
+    private void DBDayEnd()
+    {
+        SceneManager.LoadScene("S_MainMenu");
+    }
+
+    #endregion
+
+}
