157a5c2b272827ebde4a447e74a3f5740a8485b3
Destroliedel22 <Jordiwalet@gmail.com>
2026-01-07 14:12:58 +0100

    You can pull drawers out of the cabinet

--- AGENTS.md
+++ AGENTS.md
@@ -2,7 +2,7 @@
 - Project name: Pirate-Burgers
 - Unity version: Unity 6000.2.12f1
 - Active game object:
-  - Name: HandManager
+  - Name: DrawerRoot2
   - Tag: Untagged
   - Layer: Default
 <!-- UNITY CODE ASSIST INSTRUCTIONS END -->
---
+++ Assets/_DEV/Jordi/Scripts/Drawer.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+
+public class Drawer : MonoBehaviour
+{
+    [Header("Settings")]
+    public float MinDistance = 0f;
+    public float MaxDistance = 0.4f;
+
+    private Vector3 localMoveAxis = Vector3.forward;
+    private XRGrabInteractable grabInteractable;
+    private Transform drawerRoot;
+    private Vector3 beginLocalPos;
+    private Transform interactor;
+    private float beginOffset;
+
+    private void Awake()
+    {
+        grabInteractable = GetComponentInChildren<XRGrabInteractable>();
+        drawerRoot = transform.parent;
+    }
+
+    private void Start()
+    {
+        beginLocalPos = transform.localPosition;
+
+        grabInteractable.selectEntered.AddListener(OnGrab);
+        grabInteractable.selectExited.AddListener(OnRelease);
+    }
+
+    private void OnGrab(SelectEnterEventArgs args)
+    {
+        interactor = args.interactorObject.transform;
+        //converts the WorldSpace to the LocalSpace of DrawerRoot
+        Vector3 _localHandPos = drawerRoot.InverseTransformPoint(interactor.position);
+        //a float that increases if moving forward and decreases when backwards
+        beginOffset = Vector3.Dot(_localHandPos - beginLocalPos, localMoveAxis.normalized);
+    }
+
+    private void OnRelease(SelectExitEventArgs args)
+    {
+        interactor = null;
+    }
+
+    private void Update()
+    {
+        if (interactor == null)
+            return;
+        //converts the WorldSpace to the LocalSpace of DrawerRoot
+        Vector3 _localHandPos = drawerRoot.InverseTransformPoint(interactor.position);
+        //drawer cant go past minimum or maximum distance
+        float _distance = Mathf.Clamp(Vector3.Dot(_localHandPos - beginLocalPos, localMoveAxis.normalized) - beginOffset, MinDistance, MaxDistance);
+        transform.localPosition = beginLocalPos + localMoveAxis.normalized * _distance;
+    }
+}
