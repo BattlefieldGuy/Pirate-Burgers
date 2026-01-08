90b69d002c8f2721a14106de5d2fa765f9d8eca6
BattlefieldGuy <matthewklijnstra@gmail.com>
2026-01-08 12:15:13 +0100

    ! Grill adons !
    Added a addon manager to activate text set the object in place.
    fire is cooking now! once an object is cooking the fire vfx starts playing
    
    ! made adjustments to grid manager script from laura !

--- Assets/_DEV/Matthew/Scripts/GrillManager.cs
+++ Assets/_DEV/Matthew/Scripts/GrillManager.cs
@@ -1,11 +1,17 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class GrillManager : MonoBehaviour
 {
     /// <summary>
     /// This manager is responsible of hnandeling the grill grid manager and the cooking manager on every item.
     /// 
+    /// Once an item with the grill food tag enters the grill area, it will be put trough the AddFoodToGrill function. 
+    /// This function adds the item to the grilling items list to keep track of, add the item to the grill grid manager 
+    /// and enables the cooking activator on the item.
+    /// 
+    /// the cooking activater manages the cooking but also disables the rigidbody movement and rotation, and activates the text above the item.
     /// </summary>
 
 
@@ -14,12 +20,18 @@ public class GrillManager : MonoBehaviour
     public List<GameObject> grillingItems = new List<GameObject>();
 
     [SerializeField]
-    private string grillFoodTag = "GrillFood";
+    private VisualEffect grillVFX;
 
+    [SerializeField]
+    private string grillFoodTag = "GrillFood";
 
     private GrillGridManager grillGridManager;
 
+    private bool VFXOn = false;
+
     #endregion
+
+    #region --- BASIC UNITY METHODS ---
 
     void Start()
     {
@@ -28,29 +40,19 @@ public class GrillManager : MonoBehaviour
 
     void Update()
     {
-
+        VFXController();
     }
+
+    #endregion
+
+    #region --- TRIGGER HANDLERS ---
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("TSSSSSS");
         // When an item get's placed on the grill it will start cooking
         if (other.CompareTag(grillFoodTag))
         {
-            grillingItems.Add(other.gameObject);
-            Debug.Log("Added item to grill: " + other.gameObject.name);
-            CookingManager cookingManager = other.GetComponent<CookingManager>();
-            if (cookingManager != null)
-            {
-                cookingManager.enabled = true;
-                Debug.Log("Started cooking item: " + other.gameObject.name);
-            }
-
-            if (grillGridManager != null)
-            {
-                grillGridManager.AddItemToGrill(other.gameObject);
-                Debug.Log("Added item to grill grid: " + other.gameObject.name);
-            }
+            AddFoodToGrill(other.gameObject);
         }
     }
 
@@ -59,18 +61,70 @@ public class GrillManager : MonoBehaviour
         // When an item get's removed from the grill it will stop cooking
         if (grillingItems.Contains(other.gameObject))
         {
-            grillingItems.Remove(other.gameObject);
+            RemoveFoodFromGrill(other.gameObject);
+        }
+    }
 
-            CookingManager cookingManager = other.GetComponent<CookingManager>();
-            if (cookingManager != null)
+    #endregion
+
+    #region --- FOOD HANDELERS ---
+
+    private void AddFoodToGrill(GameObject _food)
+    {
+        grillingItems.Add(_food.gameObject);
+        GrillFoodActivater _grillFoodManager = _food.GetComponent<GrillFoodActivater>();
+        if (_grillFoodManager != null)
+        {
+            _grillFoodManager.Enable();
+        }
+
+        if (grillGridManager != null)
+        {
+            grillGridManager.AddItemToGrill(_food.gameObject);
+        }
+    }
+
+    private void RemoveFoodFromGrill(GameObject _food)
+    {
+        grillingItems.Remove(_food.gameObject);
+
+        GrillFoodActivater _grillFoodManager = _food.GetComponent<GrillFoodActivater>();
+        if (_grillFoodManager != null)
+        {
+            _grillFoodManager.Disable();
+        }
+
+        if (grillGridManager != null)
+        {
+            grillGridManager.RemoveItemFromGrill(_food.gameObject);
+        }
+    }
+
+    #endregion
+
+    #region --- VFX ---
+
+    private void VFXController()
+    {
+        if (grillingItems.Count > 0)
+        {
+            if (!VFXOn)
             {
-                cookingManager.enabled = false;
+                // Start grill VFX
+                grillVFX.Play();
+                VFXOn = true;
             }
-
-            if (grillGridManager != null)
+        }
+        else
+        {
+            if (VFXOn)
             {
-                grillGridManager.RemoveItemFromGrill(other.gameObject);
+                // Stop grill VFX
+                grillVFX.Stop();
+                VFXOn = false;
             }
         }
     }
+
+    #endregion
 }
