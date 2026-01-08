a335dad4d2ac4ac2ada727a3a0cae26a6ea16074
esmee veldhuizen <esmee.veldhuizen@outlook.com>
2026-01-08 13:46:23 +0100

    guys they stack but maaaannnn
    wat werkt ipv ontriggerexit
    
    het parenten doet zeer vervelend maar in theorie stackt het
    dit is zeker NIET ready voor de build nog

--- Assets/_DEV/Esmée/Scripts/Burger/StackBurger.cs
+++ Assets/_DEV/Esmée/Scripts/Burger/StackBurger.cs
@@ -2,49 +2,25 @@ using UnityEngine;
 
 public class StackBurger : MonoBehaviour
 {
-    //private void OnTriggerEnter(Collider other)
-    //{
-
-    //    Transform ingredientRoot = other.attachedRigidbody.transform;
-
-    //    ingredientRoot.SetParent(transform.parent);
-
-
-    //    //other.transform.parent = transform;
-    //    other.attachedRigidbody.isKinematic = true;
-    //    other.attachedRigidbody.useGravity = false;
-
-    //}
-
-    //void OnTriggerExit(Collider other)
-    //{
-    //    other.transform.parent = null;
-    //}
-
-
-
-
-
-    private bool occupied = false;
+    [SerializeField] private Transform snapPoint;
 
     private void OnTriggerEnter(Collider other)
     {
-        if (occupied) return;
-        if (other.attachedRigidbody == null) return;
 
-        Transform ingredient = other.attachedRigidbody.transform;
+        other.transform.SetParent(transform.parent);
+        other.transform.position = snapPoint.position;
+        //GetComponent<BoxCollider>().enabled = false;
+        Debug.Log("MIAAAUW");
 
-        ingredient.position = transform.position;
-
-        // Parent het ingredient aan DIT ingredient
-        ingredient.SetParent(transform.parent);
-
-        // Zet physics uit
-        other.attachedRigidbody.isKinematic = true;
         other.attachedRigidbody.useGravity = false;
-
-        occupied = true;
     }
+
+
+    /*ontriggerexit werkt niet met vr er moet dan iets komen wat met de exit de items unparent. ik word gek dus hou het hierbij voor nu 
+     
+     de items stacken alleen tis allemaal heel vervelend door het parenten
+    
+     bord is wel gefixt niks kan er onder parenten*/
 }
 
 
