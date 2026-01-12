200d4803eb6744539398575848e92e126d04520f
Destroliedel22 <Jordiwalet@gmail.com>
2026-01-12 11:27:43 +0100

    first drawer works completely

--- AGENTS.md
+++ AGENTS.md
@@ -2,7 +2,7 @@
 - Project name: Pirate-Burgers
 - Unity version: Unity 6000.2.12f1
 - Active game object:
-  - Name: Grabber
-  - Tag: Untagged
+  - Name: Tomato
+  - Tag: Sliceable
   - Layer: Default
 <!-- UNITY CODE ASSIST INSTRUCTIONS END -->
--- Assets/_DEV/Jordi/Scripts/DrawerStorage.cs
+++ Assets/_DEV/Jordi/Scripts/DrawerStorage.cs
@@ -1,9 +1,13 @@
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Interactables;
 
 public class DrawerStorage : MonoBehaviour
 {
     public GameObject DrawerIngredient;
+
+    [SerializeField, Range(0, -0.2f)] private float leftDistance;
+    [SerializeField, Range(0, 0.2f)] private float rightDistance;
 
     private Transform drawer;
     private GameObject spawnedObject;
@@ -25,13 +29,19 @@ public class DrawerStorage : MonoBehaviour
 
     private void OnTriggerStay(Collider other)
     {
-        Vector3 _drawerRight = drawer.right;
-        Vector3 _drawerPos = this.transform.position;
-        Vector3 _drawerToHand = handInTrigger.transform.position - _drawerPos;
-        float _distanceAlongRight = Vector3.Dot(_drawerToHand, _drawerRight);
+        if (other.CompareTag("RealHand"))
+        {
+            Vector3 _drawerRight = drawer.right;
+            Vector3 _drawerPos = this.transform.position;
+            Vector3 _drawerToHand = handInTrigger.transform.position - _drawerPos;
+            float _distanceAlongRight = Vector3.Dot(_drawerToHand, _drawerRight);
 
-        if(spawnedObject)
-            spawnedObject.transform.position = _drawerPos + _drawerRight * _distanceAlongRight;
+            if (spawnedObject)
+            {
+                float _distance = Mathf.Clamp(_distanceAlongRight, leftDistance, rightDistance);
+                spawnedObject.transform.position = _drawerPos + _drawerRight * _distance;
+            }
+        }
     }
 
     private void OnTriggerExit(Collider other)
@@ -39,9 +49,15 @@ public class DrawerStorage : MonoBehaviour
         if (other.CompareTag("RealHand"))
         {
             if (spawnedObject.GetComponent<XRGrabInteractable>().isSelected == true)
+            {
+                Rigidbody _rb = spawnedObject.GetComponent<Rigidbody>();
+                spawnedObject.GetComponent<XRGrabInteractable>().selectExited.AddListener(_ => _rb.isKinematic = false);
                 spawnedObject = null;
+            }
             else
                 Destroy(spawnedObject);
+
+            handInTrigger = null;
         }
     }
 }
