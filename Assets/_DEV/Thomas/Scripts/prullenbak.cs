ef12e387a9bcf4341790dab0a16106c2701babaf
esmee veldhuizen <esmee.veldhuizen@outlook.com>
2026-01-22 09:54:18 +0100

    meat should cook
    maybe trashcan but idk bro

--- Assets/_DEV/Thomas/Scripts/prullenbak.cs
+++ Assets/_DEV/Thomas/Scripts/prullenbak.cs
@@ -1,10 +1,10 @@
-using System;
 using UnityEngine;
 
 public class prullenbak : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        Destroy(other.gameObject);
+        if (!other.CompareTag("RealHand") || !other.CompareTag("HandAttachable"))
+            Destroy(other.gameObject);
     }
 }
